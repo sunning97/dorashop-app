712a67888ea80c63128dc859c9d7ab92700bfed0
sunning97 <mrcatbro97@gmail.com>
2018-08-31 10:22:53 +0700

    update

--- app/src/main/java/com/neko/giangnguyen/dorashop/View/home/HomeActivity.java
+++ app/src/main/java/com/neko/giangnguyen/dorashop/View/home/HomeActivity.java
@@ -3,6 +3,8 @@ package com.neko.giangnguyen.dorashop.View.home;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -18,17 +20,33 @@ public class HomeActivity extends AppCompatActivity {
     ViewPager viewPager;
     ViewPagerAdapter viewPagerAdapter;
 
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
 
+        drawerLayout = findViewById(R.id.home_draw_layout);
+
+
         tabLayout = findViewById(R.id.home_tablayout);
         viewPager = findViewById(R.id.home_view_pager);
-        toolbar = (Toolbar) findViewById(R.id.home_bar);
+        toolbar = findViewById(R.id.home_bar);
+
+
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        actionBarDrawerToggle.syncState();
+
         viewPager.setAdapter(viewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
     }
@@ -42,6 +60,7 @@ public class HomeActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)) return true;
         return super.onOptionsItemSelected(item);
     }
 }
