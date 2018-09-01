7b37d06255251d495e4ca5f5901c6c0c145f3cf3
sunning97 <mrcatbro97@gmail.com>
2018-09-01 18:11:00 +0700

    update

---
+++ app/src/main/java/com/neko/giangnguyen/dorashop/CustomView/LoginPassword.java
@@ -0,0 +1,75 @@
+package com.neko.giangnguyen.dorashop.CustomView;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.text.InputType;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.widget.EditText;
+import com.neko.giangnguyen.dorashop.R;
+
+@SuppressLint("AppCompatCustomView")
+public class LoginPassword extends EditText{
+    Drawable eyeVisible,eyeHidden;
+
+    Boolean isPasswordVisible = false;
+    Boolean eye = false;
+
+    public LoginPassword(Context context) {
+        super(context);
+        this.inital(null);
+    }
+
+    public LoginPassword(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.inital(attrs);
+    }
+
+    public LoginPassword(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.inital(attrs);
+    }
+
+    public LoginPassword(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        this.inital(attrs);
+    }
+
+    private void inital(AttributeSet attributeSet){
+
+        if(attributeSet != null){
+            TypedArray typedArray = getContext()
+                    .getTheme()
+                    .obtainStyledAttributes(attributeSet,R.styleable.LoginPassword,0,0);
+            this.eye = typedArray.getBoolean(R.styleable.LoginPassword_eyeHidden,false);
+
+            typedArray.recycle();
+        }
+        eyeHidden = ContextCompat.getDrawable(getContext(),R.drawable.visibility_black);
+        eyeVisible = ContextCompat.getDrawable(getContext(),R.drawable.visibility_off_black);
+
+        this.setIcon();
+    }
+
+    private void setIcon(){
+        setInputType(InputType.TYPE_CLASS_TEXT | (this.isPasswordVisible ? InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD : InputType.TYPE_TEXT_VARIATION_PASSWORD));
+        Drawable[] drawables = getCompoundDrawables();
+        Drawable drawable = this.eye && !this.isPasswordVisible ? this.eyeVisible : this.eyeHidden;
+        drawable.setAlpha((int) (255 * .60f));
+        setCompoundDrawablesRelativeWithIntrinsicBounds(drawables[0],drawables[1],drawable,drawables[3]);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN && event.getX() >= (getRight() - getCompoundDrawables()[2].getBounds().width())){
+            this.isPasswordVisible = !this.isPasswordVisible;
+            this.setIcon();
+            invalidate();
+        }
+        return super.onTouchEvent(event);
+    }
+}
